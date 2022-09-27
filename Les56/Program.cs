b1b6773cb77a0a49cae60459f799806fe1296ec4
Vrezh <vrezhikmarkaryan@gmail.com>
2022-09-27 19:27:46 +0500

    les56

---
+++ Les56/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+// находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер 
+// строки с наименьшей суммой элементов: 1 строка
+Console.Clear();
+
+void PrintArrya(int[,] arg) 
+{
+    for (int i = 0; i < arg.GetLength(0); i++) 
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)  
+        {
+            Console.Write($"{arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrya(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++) 
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)  
+        {
+            arg[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void SummaArray(int[,] arg)
+{
+    int sumRow = 0;
+    int sumColumn = 0;
+    int sum = 0;
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        sumColumn = 0;
+        sumRow = 0;
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            sumRow += arg[i, j];
+            sumColumn += arg[j, i];
+            sum += arg[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки {sumRow} и сумма {i + 1} колоны  {sumColumn}");
+    }
+    Console.WriteLine($"Обшая сумма элементов двумерного массива {sum}");
+}
+
+Console.Write("Введите длину строки ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину столбца ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[row, colums];
+
+FillArrya(arr);
+PrintArrya(arr);
+
+SummaArray(arr);
