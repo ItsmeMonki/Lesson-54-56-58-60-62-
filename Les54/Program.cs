b1a3d06440b494d01f1cbc57082d62587e8a6f33
Vrezh <vrezhikmarkaryan@gmail.com>
2022-09-27 19:26:33 +0500

    les54

---
+++ Les54/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+Console.Clear();
+
+void PrintArrya(int[,] arg)   // Метод вывода матрицы
+{
+    for (int i = 0; i < arg.GetLength(0); i++) // rows - строки
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)  // columns - столбцы
+        {
+            Console.Write($"{arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArrya(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            arg[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void SortArrayMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            for (int k = 0; k < arg.GetLength(0); k++)
+            {
+                for (int n = 0; n < arg.GetLength(1); n++)
+                {
+                    if (arg[i, j] < arg[k, n])
+                    {
+                        int h = arg[i, j];
+                        arg[i, j] = arg[k, n];
+                        arg[k, n] = h;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+Console.Write("Введите длину строки ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите длину столбца ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[row, colums];
+
+FillArrya(arr);
+PrintArrya(arr);
+Console.WriteLine();
+SortArrayMatrix(arr);
+PrintArrya(arr);
